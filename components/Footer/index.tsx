429cd0652fe509a12e63b4ca5c4bdf7856c22f88
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-10-25 22:32:23 +0800

    feat: add (hopefully temporary) footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,9 +1,21 @@
-// import styles from './index.module.scss'
+import styles from './index.module.scss'
 
 const Footer = () => {
   return (
-    <footer>
-      Footer
+    <footer className={styles.footer}>
+      <div className={styles.info}></div>
+      <div className={styles.detailsBox}>
+        <span className={styles.detailsHeader}>Discover</span>
+        <a className={styles.details}>About Us</a>
+        <a className={styles.details}>Our Commitment</a>
+        <a className={styles.details}>Contact</a>
+      </div>
+      <div className={styles.detailsBox}>
+        <span className={styles.detailsHeader}>Reach Out</span>
+        <a className={styles.details}>Address</a>
+        <a className={styles.details}>Telephone</a>
+        <a className={styles.details}>Email</a>
+      </div>
     </footer>
   )
 }
