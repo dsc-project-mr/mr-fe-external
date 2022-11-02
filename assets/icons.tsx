c60d3319b3f75e0c5b77853597a10a9699ae2764
firwer <firwer@gmail.com>
2022-11-02 23:06:05 +0800

    Changed to interface to fix default issue

--- assets/icons.tsx
+++ assets/icons.tsx
@@ -1,12 +1,25 @@
-export const MRIcon = (props: {
-  icon: any
-  width: 50
-  height: 50
-  color: 'black'
-}) => {
-  return (
-    <props.icon
-      style={{ width: props.width, height: props.height, color: props.color }}
-    />
-  )
+// Required props
+interface IconRequiredProps {
+  Icon: any
 }
+// Optional props
+interface IconOptionalProps {
+  width: number
+  height: number
+  color: string
+}
+// Combine required and optional props to build the full prop interface
+interface IconProps extends IconRequiredProps, IconOptionalProps {}
+// Use the optional prop interface to define the default props
+const defaultProps: IconOptionalProps = {
+  width: 50,
+  height: 50,
+  color: 'red',
+}
+// Use the full props within the actual component
+export const MRIcon = (props: IconProps) => {
+  const { Icon, width, height, color } = props
+  return <Icon style={{ width: width, height: height, color: color }} />
+}
+// Be sure to set the default props
+MRIcon.defaultProps = defaultProps
