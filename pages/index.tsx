fcbe00db4562d78d8b7ce3e76857c6ec4e6bc613
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-11-07 18:48:06 +0800

    feat: move component demo code to it's own page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,9 @@
 import type { NextPage } from 'next'
 
-import Breadcrumb from 'components/Breadcrumb'
-import Breadcrumbs from 'components/Breadcrumbs'
-import { Box } from '@mui/material'
-import { useState } from 'react'
-
 const Home: NextPage = () => {
-  const [value, setValue] = useState(0)
   return (
     <div>
-      <Box>
-        <Breadcrumbs value={value} onChange={(_, v) => setValue(v)}>
-          <Breadcrumb label="Select Campaign" />
-          <Breadcrumb label="Select Payment" />
-          <Breadcrumb label="Donation Amount" />
-          <Breadcrumb label="My Details" />
-          <Breadcrumb label="Submit" />
-        </Breadcrumbs>
-      </Box>
-      <div hidden={value != 0}>Item One</div>
-      <div hidden={value != 1}>Item Two</div>
-      <div hidden={value != 2}>Item Three</div>
-      <div hidden={value != 3}>Item Four</div>
-      <div hidden={value != 4}>Item Five</div>
+      <h1>Mercy Relief Portal</h1>
     </div>
   )
 }
