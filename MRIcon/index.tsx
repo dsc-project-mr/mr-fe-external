da5a9a350a85685a226d6f2c79df7f9e0288d4a3
firwer <firwer@gmail.com>
2022-11-30 15:53:50 +0800

    Code revision

--- MRIcon/index.tsx
+++ MRIcon/index.tsx
@@ -4,12 +4,13 @@ interface IconRequiredProps {
 }
 // Optional props
 interface IconOptionalProps {
-  width: number
-  height: number
-  color: string
+  width?: number
+  height?: number
+  color?: string
 }
 // Combine required and optional props to build the full prop interface
-interface IconProps extends IconRequiredProps, IconOptionalProps {}
+type IconProps = IconRequiredProps & IconOptionalProps
+
 // Use the optional prop interface to define the default props
 const defaultProps: IconOptionalProps = {
   width: 50,
@@ -17,9 +18,12 @@ const defaultProps: IconOptionalProps = {
   color: 'black',
 }
 // Use the full props within the actual component
-export const MRIcon = (props: IconProps) => {
+const MRIcon = (props: IconProps) => {
   const { Icon, width, height, color } = props
   return <Icon style={{ width: width, height: height, color: color }} />
 }
+
+export default MRIcon
+
 // Be sure to set the default props
 MRIcon.defaultProps = defaultProps
