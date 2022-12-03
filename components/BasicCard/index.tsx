5e65cefbb28958d04c17ece677240b61f31d486d
Li Hui Cham <lihuicham@gmail.com>
2022-12-03 22:27:50 +0800

    feat: complete basic card component

--- components/BasicCard/index.tsx
+++ components/BasicCard/index.tsx
@@ -1,41 +1,89 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+// const BasicCard = () => {
+//   return (
+//     <Card sx={{ maxWidth: 345 }}>
+//       <CardMedia
+//         component="img"
+//         height="140"
+//         image="/static/images/cards/contemplative-reptile.jpg"
+//         alt="green iguana"
+//       />
+//       <CardContent>
+//         <Typography gutterBottom variant="h5" component="div">
+//           Lizard
+//         </Typography>
+//         <Typography variant="body2" color="text.secondary">
+//           Lizards are a widespread group of squamate reptiles, with over 6,000
+//           species, ranging across all continents except Antarctica
+//         </Typography>
+//       </CardContent>
+//       <CardActions>
+//         <Button size="small">Share</Button>
+//         <Button size="small">Learn More</Button>
+//       </CardActions>
+//     </Card>
+//   );
+// }
+
+// export default BasicCard;
 
 const BasicCard = () => {
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
+    <Card sx = {{ 
+      maxWidth: 500, 
+      maxHeight: 500, 
+      background: "#FFFFFF",
+      boxShadow: "0px 0px 0px 1px #E0E0E0",
+      borderRadius: "25px",
+      display:'flex',
+      flexDirection:"column",
+      justifyContent:"center",
+      alignItems: "center",
+      margin: 10
+      }}>
+      <CardContent
+      sx={{ 
+        display:'flex',
+        flexDirection:"column",
+        justifyContent:"center",
+        alignItems: "center",
+      }}>
+        <CardMedia
+            alt="fundraise-img"
+            component="img"
+            title="Fundraise Image"
+            height="140"
+            image='/test.jpeg'
+        />
+        <Typography variant="h5" gutterBottom>
+          FUNDRAISE FOR US
         </Typography>
         <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus voluptas harum, porro molestiae numquam a
+          nimi a vero neque corrupti ipsam dolorem id iure aperiam fugit necessitatibus soluta corporis doloremque tenetur.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus voluptas harum, porro molestiae numquam a
+          nimi a vero neque corrupti ipsam dolorem id iure aperiam fugit necessitatibus soluta corporis doloremque tenetur.
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button 
+        color="info"
+        variant='contained'
+        onClick={() => {
+          alert('clicked')
+        }}
+        sx={{
+          marginBottom: "10px"
+        }}>
+          SPONSOR NOW
+        </Button>
       </CardActions>
     </Card>
   );
