946dec930afc5e2046eb22f47c290690c0c71966
Li Hui Cham <lihuicham@gmail.com>
2022-12-19 23:38:48 +0800

    Fix static image importing and rendering

--- components/BasicCard/index.tsx
+++ components/BasicCard/index.tsx
@@ -5,34 +5,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
-// const BasicCard = () => {
-//   return (
-//     <Card sx={{ maxWidth: 345 }}>
-//       <CardMedia
-//         component="img"
-//         height="140"
-//         image="/static/images/cards/contemplative-reptile.jpg"
-//         alt="green iguana"
-//       />
-//       <CardContent>
-//         <Typography gutterBottom variant="h5" component="div">
-//           Lizard
-//         </Typography>
-//         <Typography variant="body2" color="text.secondary">
-//           Lizards are a widespread group of squamate reptiles, with over 6,000
-//           species, ranging across all continents except Antarctica
-//         </Typography>
-//       </CardContent>
-//       <CardActions>
-//         <Button size="small">Share</Button>
-//         <Button size="small">Learn More</Button>
-//       </CardActions>
-//     </Card>
-//   );
-// }
-
-// export default BasicCard;
+import Image from 'next/image';
 
 const BasicCard = () => {
   return (
@@ -55,13 +28,16 @@ const BasicCard = () => {
         justifyContent:"center",
         alignItems: "center",
       }}>
-        <CardMedia
+        <CardMedia title="Fundraise Image">
+          <Image src="/test.jpeg" height="114" width="114" alt="" />
+        </CardMedia>
+        {/* <CardMedia
             alt="fundraise-img"
             component="img"
             title="Fundraise Image"
             height="140"
             image='/test.jpeg'
-        />
+        /> */}
         <Typography variant="h5" gutterBottom>
           FUNDRAISE FOR US
         </Typography>
